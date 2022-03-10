bd7db076e557d68bc06c8ab0a4aa9e5183d46afa
yuze <looker53@sina.com>
2022-03-10 21:42:55 +0800

    new post

--- dp.sh
+++ dp.sh
@@ -1,6 +1,6 @@
 git add -A 
 git commit -m 'new post'
-git push git@github.com:looker53/blog.git master
+git push git@github.com:looker53/blog.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
